e140cdff411cb9737b645b9b4c8d3b4c99c9cf00
Javier <javiercastanocandela@hotmail.com>
2021-11-24 23:24:17 +0100

    data prep doc

---
+++ web_scraping/project_dir/myproject/spiders/IP_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 15:58:45 2021
+
+@author: javier
+"""
+
+#from scrapy import Spider  # CrawlSpider, Rule
+#from scrapy.linkextractors import LinkExtractor
+#from scrapy.loader import ItemLoader 
+#from scrapy.loader.processors import MapCompose
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+def get_ip_list():
+    url = 'https://free-proxy-list.net/'
+    
+    soup = bs(requests.get(url).content, 'html.parser')
+              
+    proxies = []
+    for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+        tds = row.find_all('td')
+        try:
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            proxies.append(str(ip) + ':' + str(port))
+        except IndexError:
+            continue
+        
+    return proxies
+
+ip_list_path = '/home/javier/Desktop/TFM/Fraud_Detection_In_The_Irish_Rental_Market/web_scraping/project_dir/proxies.txt'
+with open(ip_list_path, 'w') as file:
+    for proxy in get_ip_list():
+        file.write(proxy + '\n')
+
---
+++ web_scraping/project_dir/run_scrapy_script.py
@@ -0,0 +1,28 @@
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+#from myproject.spiders.rent import RentIeSpider
+from myproject.spiders.daft import DaftSpider
+#from myproject.spiders.property import PropertySpider
+
+from scrapy.signalmanager import dispatcher
+
+
+def spider_results():
+    results = []
+
+    def crawler_results(signal, sender, item, response, spider):
+        results.append(item)
+
+    dispatcher.connect(crawler_results, signal=signals.item_passed)
+
+    process = CrawlerProcess(get_project_settings())
+    #process.crawl(RentIeSpider)
+    process.crawl(DaftSpider)
+    #process.crawl(PropertySpider)
+    process.start()  # the script will block here until the crawling is finished
+    return results
+
+spider_results()
+#if __name__ == '__main__':
+ #   print(spider_results())
