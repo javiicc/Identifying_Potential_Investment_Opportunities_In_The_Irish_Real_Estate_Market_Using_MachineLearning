bb68f0edc8730c588f053341a4cd55058b64012c
Javier <javiercastanocandela@hotmail.com>
2022-01-19 16:58:45 +0100

    img

--- dashapp/app.py
+++ dashapp/app.py
@@ -1,5 +1,5 @@
 # Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+# visit http://127.0.0.1:3000/ in your web browser.
 
 import dash
 from dash import dcc
@@ -10,8 +10,8 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX],
-       #         meta_tags=[{'name': 'viewport',
-        #                    'content': 'width=device-width, initial-scale=1.0'}]
+               # meta_tags=[{'name': 'viewport',
+                #            'content': 'width=device-width, initial-scale=1.0'}]
                 )
 
 df = pd.read_csv('../investment-opportunities/data/07_model_output/data_for_frontend.csv',
@@ -20,21 +20,21 @@ df['predicted_price'] = df['predicted_price'].round(decimals=0)
 df['residual'] = df['residual'].round(decimals=0)
 
 
-
-
-
-
 # Layout section: Boostrap
 # ------------------------------------------------------------------
 app.layout = dbc.Container([
 
+    # First row (height 15)
     dbc.Row([
+        # Column (total width 12)
+        # Title
         dbc.Col(html.H1("Ireland's Real Estate Market Opportunities",
                         className='pt-4 text-center text-primary text-light',
                         ),
                 width=12,
                 style={"backgroundColor": "#100508"},
                 ),
+        # Explanation bellow the title
         dcc.Markdown('''
         Welcome to Javier Castaño's final project from the Master in Data Science at
         KSchool. You can interact with the map through the controls bellow.''',
@@ -45,10 +45,12 @@ app.layout = dbc.Container([
         style={"height": "15vh"}
     ),
 
+    # Second row (height 80)
+    # Contains 3 columns
     dbc.Row([
-
+        # First column (width 8 of 12)
         dbc.Col([
-
+            # Map graph
             dcc.Graph(
                 id='map',
                 responsive=True,
@@ -58,18 +60,20 @@ app.layout = dbc.Container([
             width={'size': 8},
             style={"backgroundColor": "#100508"}
         ),
-
+        # Second column (width 2 of 12)
         dbc.Col([
-
+            # Bar graph
             dcc.Graph(
                 id='bar',
             ),
+            # Markdown bellow bar graph -> price_range_slider title
             dcc.Markdown(
                 '''
                 **PRICE**
                 ''',
                 className="text-center my-5",
             ),
+            # Range slider for price variable
             dcc.RangeSlider(
                 id='price_range_slider',
                 className='my-sm-5',
@@ -88,12 +92,14 @@ app.layout = dbc.Container([
                         'style': {'color': '#f50'}}},
                 tooltip={"placement": "top", "always_visible": False}
             ),
+            # Markdown  -> residuals_range_slider title
             dcc.Markdown(
                 '''
                 **RESIDUALS**
                 ''',
                 className="text-center",
             ),
+            # Range slider for residuals variable
             dcc.RangeSlider(
                 id='residuals_range_slider',
                 className='my-sm-5',
@@ -116,22 +122,20 @@ app.layout = dbc.Container([
             width={'size': 2},
             style={"backgroundColor": "#100508"}
         ),
-
-
-
+        # Third column (width 2 of 12)
         dbc.Col([
-
+            # Pie graph
             dcc.Graph(
                 id='pie',
             ),
-
+            # Markdown bellow pie graph -> floor_area_slider title
             dcc.Markdown(
                 '''
                 **FLOOR AREA**
                 ''',
                 className="text-center my-5",
             ),
-
+            # Range slider for floor_area variable
             dcc.RangeSlider(
                 id='floor_area_slider',
                 className='my-sm-5',
@@ -152,12 +156,14 @@ app.layout = dbc.Container([
                         'style': {'color': '#f50'}}},
                 tooltip={"placement": "top", "always_visible": False}
             ),
+            # Markdown -> bedroom_range_slider title
             dcc.Markdown(
                 '''
                 **BEDROOMS**
                 ''',
                 className="text-center",
             ),
+            # Range slider for bedroom variable
             dcc.RangeSlider(
                 id='bedroom_range_slider',
                 className='my-sm-3',
@@ -167,12 +173,14 @@ app.layout = dbc.Container([
                 max=df.bedroom.max(),
                 value=[df.bedroom.min(), df.bedroom.max()]
             ),
+            # Markdown -> bathroom_range_slider title
             dcc.Markdown(
                 '''
                 **BATHROOMS**
                 ''',
                 className="text-center",
             ),
+            # Range slider for bathroom variable
             dcc.RangeSlider(
                 id='bathroom_range_slider',
                 className='my-sm-3',
@@ -189,23 +197,23 @@ app.layout = dbc.Container([
     ],
         style={"height": "80vh"}
     ),
+    # Third row (height 5)
     dbc.Row([
-        dbc.Col(html.H6("",
-                        className='text-center'),
+        # Empty column (width 8 of 12)
+        dbc.Col(
                 width=8,
                 style={"backgroundColor": "#100508"}
                 ),
+        # Second column (width 4 of 12)
         dbc.Col(
+            # Link to my LinkedIn ;)
             dcc.Link(
                 'LinkedIn',
                 href='https://www.linkedin.com/in/javier-casta%C3%B1o-candela-b89039208/',
                 className="text-center",
-                style={"backgroundColor": "#100508", 'color': '#77C6FB'},  # , 'color': 'white
+                style={"backgroundColor": "#100508", 'color': '#77C6FB'},
                 refresh=True,
             ),
-    #        html.H1("",
-     #                   className='pt-4 text-center text-primary text-light',
-      #                  ),
             width=4,
             style={"backgroundColor": "#100508"}
                 ),
@@ -221,6 +229,10 @@ app.layout = dbc.Container([
 # Callback section: Boostrap
 # ------------------------------------------------------------------
 
+#  By writing this decorator, we're telling Dash to call this function for us whenever
+#  the value of the "input" component changes in order to update the children of
+#  the "output" component on the page
+
 
 @app.callback(
     Output(component_id='map', component_property='figure'),
@@ -234,7 +246,7 @@ app.layout = dbc.Container([
 )
 def update_output_map(price_range, floor_area_range, residuals_range, bedroom_range,
                       bathroom_range):
-
+    # Filter the DataFrame according to the inputs
     filtered_df = df[
         (df.actual_price >= price_range[0])
         & (df.actual_price <= price_range[1])
@@ -247,7 +259,7 @@ def update_output_map(price_range, floor_area_range, residuals_range, bedroom_ra
         & (df.bathroom >= bathroom_range[0])
         & (df.bathroom <= bathroom_range[1])
         ]
-
+    # Map figure with the filtered DataFrame
     map_fig = px.scatter_mapbox(
         filtered_df,
         lat="latitude",
@@ -275,20 +287,21 @@ def update_output_map(price_range, floor_area_range, residuals_range, bedroom_ra
         template='plotly_dark',
     )
     map_fig.update(layout_coloraxis_showscale=False)
-
+    # Pie figure with the filtered DataFrame
     pie_fig = px.pie(
         filtered_df,
         values='actual_price',
         names='type_house',
-        # title='',
         color_discrete_sequence=px.colors.sequential.RdBu,
         hover_data={'type_house': False,
                     'actual_price': False},
         template='plotly_dark',
     )
-
+    # Mean price of the five places with higher mean prices according to the
+    # filtered DataFrame
     mean_df = filtered_df.groupby('place')['actual_price'].mean()\
                          .sort_values(ascending=False).head()
+    # Bar figure with DataFrame above
     bar_fig = px.bar(
         mean_df,
         x=mean_df.index,
